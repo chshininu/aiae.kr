6bfe3c833171e3f467ae2e0c14810e5aed037dde
chshininu <chshin@inu.ac.kr>
2026-04-26 20:02:48 +0900

    Fix App.jsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
-function Router() {
+function Routes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -20,7 +20,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        {/* Router에 base 경로 추가 */}
+        <Router base="/aiae.kr">
+          <Routes />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
