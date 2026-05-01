9aa827763f55f02a2f51925b2a2f45d8763bcb13
chshininu <chshin@inu.ac.kr>
2026-05-01 20:19:37 +0900

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: '/aiae.kr/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
